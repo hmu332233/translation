7383250008054fad24a2233e82e52ef98c453ecc
minung.han <hmu332233@gmail.com>
2020-04-07 00:48:42 +0900

    feat: header 추가

--- src/client/pages/Main/Main.jsx
+++ src/client/pages/Main/Main.jsx
@@ -9,15 +9,24 @@ import TextareaGroupList from 'containers/TextareaGroupList';
 
 function Main(props) {
   return (
-    <div className={classNames('container', styles.Main)}>
-      <div className="row">
-        <div className="col-12">
-          <LanguageDropdown />
+    <div className={styles.Main}>
+      <header>
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            Translation
+          </a>
+        </nav>
+      </header>
+      <div className={classNames('container', styles.Main__body)}>
+        <div className="row">
+          <div className="col-12">
+            <LanguageDropdown />
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-12">
-          <TextareaGroupList className="w-100" />
+        <div className="row mt-3">
+          <div className="col-12">
+            <TextareaGroupList className="w-100" />
+          </div>
         </div>
       </div>
     </div>
