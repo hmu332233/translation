97233ce7ff19d04b17ff190c2948262b42e2501b
minung.han <hmu332233@gmail.com>
2020-09-24 00:00:03 +0900

    feat: 번역 결과에서 동기적으로 동작하던 로직 변경

--- src/server/controllers/translation/index.js
+++ src/server/controllers/translation/index.js
@@ -4,8 +4,10 @@ const naverApi = require('../../utils/api/naverApi');
 exports.translate = async (req, res, next) => {
   const { text, source, target } = req.query;
   try {
-    const kakaoTranslatedText = await kakaoApi.translate({ text, source, target });
-    const naverTranslatedText = await naverApi.translate({ text, source, target });
+    const [kakaoTranslatedText, naverTranslatedText] = await Promise.all([
+      kakaoApi.translate({ text, source, target }),
+      naverApi.translate({ text, source, target })
+    ])
     res.json({
       kakao: kakaoTranslatedText.join('\n'),
       naver: naverTranslatedText,
