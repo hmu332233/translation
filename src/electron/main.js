2ad0f7015a96d257a568cddd6702c199533b03e1
minung.han <hmu332233@gmail.com>
2020-10-13 01:12:01 +0900

    feat: electron 셋팅 추가

---
+++ src/electron/main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  // 브라우저 창을 생성합니다.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('./../../public/index.html');
+}
+
+app.whenReady().then(createWindow);
