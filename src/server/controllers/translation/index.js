09b00d0fdad6c7325d24e0ac63c8dd80a3bd6f78
minung.han <hmu332233@gmail.com>
2021-03-07 22:52:35 +0900

    feat: 번역 api 요청시 allSettled 사용하도록 변경

--- src/server/controllers/translation/index.js
+++ src/server/controllers/translation/index.js
@@ -2,18 +2,31 @@ const kakaoApi = require('../../utils/api/kakaoApi');
 const naverApi = require('../../utils/api/naverApi');
 const googleApi = require('../../utils/api/googleApi');
 
+const normalizeResults = results => {
+  if (results.status === 'rejected') {
+    return '에러';
+  }
+   
+  if (Array.isArray(results.value)) {
+    return results.value.join('\n');
+  }
+
+  return results.value;
+};
+
 exports.translate = async (req, res, next) => {
   const { text, source, target } = req.query;
   try {
-    const [kakaoTranslatedText, naverTranslatedText, googleTranslatedText] = await Promise.all([
+    const [kakaoResults, naverResults, googleResults] = await Promise.allSettled([
       kakaoApi.translate({ text, source, target }),
       naverApi.translate({ text, source, target }),
       googleApi.translate({ text, source, target }),
-    ])
+    ]);
+
     res.json({
-      kakao: kakaoTranslatedText.join('\n'),
-      naver: naverTranslatedText,
-      google: googleTranslatedText,
+      kakao: normalizeResults(kakaoResults),
+      naver: normalizeResults(naverResults),
+      google: normalizeResults(googleResults),
     });
   } catch (err) {
     next(err);
